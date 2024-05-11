a18296381f3bbe675e7465ec428225a66c2a832f
YohannesTg <111312883+YohannesTg@users.noreply.github.com>
2024-05-11 15:08:51 +0300

    Delete Game directory

--- Game/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-$BOT_TOKEN="7004677225:AAEvp1tFSgiIXkQqkCp5mkB6tRScWhcdnAs"
-
-?>
--- Game/deletewebhook.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'config.php';
-$webHookUrl="https://phptestregistration.000webhostapp.com/index.php";
-$apiUrl="https://api.telegram.org/bot{$BOT_TOKEN}/deleteWebhook";
-$response=file_get_contents($apiUrl);
-echo $apiUrl;
