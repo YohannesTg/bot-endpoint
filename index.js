d84d61dc9bfb8a67e26ba2a540afb1d486d753d4
YohannesTg <111312883+YohannesTg@users.noreply.github.com>
2024-05-12 05:19:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Excuted")
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  console.log('Request received');
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
