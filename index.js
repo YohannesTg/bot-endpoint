bb60231787b24c2a343a78a01188ae7016e851e3
YohannesTg <111312883+YohannesTg@users.noreply.github.com>
2024-05-12 18:22:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,12 +14,26 @@ app.post('/telegram', (req, res) => {
   const chatId = jsonData.message.chat.id;
   const message = jsonData.message.text; // Get the text of the received message
 
-  // Send the received message back to the user
-  bot.sendMessage(chatId, message);
+  // Create an inline keyboard with a button
+  const keyboard = {
+    inline_keyboard: [
+      [
+        {
+          text: 'Click me',
+          callback_data: 'button_clicked'
+        }
+      ]
+    ]
+  };
+
+  // Send the received message back to the user with the inline keyboard
+  bot.sendMessage(chatId, message, {
+    reply_markup: JSON.stringify(keyboard)
+  });
+
   res.sendStatus(200); // Send a success response
 });
 
-
 app.listen(8443, () => {
-  console.log('Express server is running on port 3000');
+  console.log('Express server is running on port 8443');
 });
