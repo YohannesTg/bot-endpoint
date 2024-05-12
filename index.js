b3ae441d00770fdb1bdd9c826e0dd698e6b0877d
YohannesTg <111312883+YohannesTg@users.noreply.github.com>
2024-05-12 17:50:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 
 // Set up your bot token
 const botToken = '7004677225:AAH_qVX9NO0CRxpMnw0t1Jz52ez9HqunN9I';
-const bot = new TelegramBot(botToken, { polling: false });
+const bot = new TelegramBot(botToken, { polling: true });
 
 app.post('/telegram', (req, res) => {
   const jsonData = req.body;
