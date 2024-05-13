3f9936b02581ebeabe69830d83ad22cccdafb270
YohannesTg <111312883+YohannesTg@users.noreply.github.com>
2024-05-13 20:45:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const TelegramBot = require('node-telegram-bot-api');
+const { Telegraf } = require('telegraf');
 
 const app = express();
 app.use(bodyParser.json());
 
-// Set up your bot token
 const botToken = '7004677225:AAH_qVX9NO0CRxpMnw0t1Jz52ez9HqunN9I';
-const bot = new TelegramBot(botToken, { polling: false });
-
+const bot = new Telegraf(botToken);
 
 app.post('/telegram', (req, res) => {
   const jsonData = req.body;
-  const chatId = jsonData.message.chat.id;
-  const message = jsonData.message.text; // Get the text of the received message
-
-  // Create an inline keyboard with a button
-  const keyboard = {
-    inline_keyboard: [
-      [
-        {
-          text: 'Click me',
-          callback_data: 'button_clicked'
-        }
-      ]
-    ]
-  };
-
-  // Send the received message back to the user with the inline keyboard
-  bot.sendMessage(chatId, message, {
-    reply_markup: JSON.stringify(keyboard)
-  });
-
-  res.sendStatus(200); // Send a success response
+  bot.handleUpdate(jsonData);
+  res.sendStatus(200);
 });
 
 app.listen(8443, () => {
