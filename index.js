866852492982cf35d658b54a94a50c71d645538b
YohannesTg <111312883+YohannesTg@users.noreply.github.com>
2024-05-13 22:59:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ const botToken = '7004677225:AAH_qVX9NO0CRxpMnw0t1Jz52ez9HqunN9I';
 const bot = new Telegraf(botToken);
 
 // Set up the webhook URL
-const webhookUrl = 'https://telegame.vercel.app';
-
+const webhookUrl = 'web url
 // Set up the webhook route
 app.post(`/webhook/${botToken}`, (req, res) => {
   bot.handleUpdate(req.body);
