4cb4f671aea92e7d39bcb234dfb2c74b65d2bf97
YohannesTg <111312883+YohannesTg@users.noreply.github.com>
2024-05-16 22:48:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,22 @@ bot.telegram.setWebhook(webhookUrl);
 bot.startWebhook(`/webhook/${botToken}`, null, 8443);
 
 // Handle incoming text messages
+bot.command("inline", (ctx) => {
+    ctx.reply("Hi there!", {
+        reply_markup: {
+            inline_keyboard: [
+                /* Inline buttons. 2 side-by-side */
+                [ { text: "Button 1", callback_data: "btn-1" }, { text: "Button 2", callback_data: "btn-2" } ],
+
+                /* One button */
+                [ { text: "Next", callback_data: "next" } ],
+                
+                /* Also, we can have URL buttons. */
+                [ { text: "Open in browser", url: "telegraf.js.org" } ]
+            ]
+        }
+    });
+});
 bot.on('inline_query', async (ctx) => {
   const query = ctx.inlineQuery.query;
 
