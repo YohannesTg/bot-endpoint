33d411b495dc5a44ca0821ea50e2ef54b6ba3551
YohannesTg <111312883+YohannesTg@users.noreply.github.com>
2024-05-19 19:38:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ bot.on('callback_query', async (ctx) => {
   const callbackQueryId = ctx.callbackQuery.id;
 
   // Answer the callback query with the game URL
-  await ctx.answerCallbackQuery(callbackQueryId, gameUrl);
+  await ctx.answerCbQuery(gameUrl);
 });
 
 app.listen(8443, () => {
