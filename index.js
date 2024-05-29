06b98b5b0f88ff920ce93a79de232728aac260e9
YohannesTg <111312883+YohannesTg@users.noreply.github.com>
2024-05-29 21:18:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -61,10 +61,11 @@ bot.on('inline_query', (ctx) => {
   return ctx.answerInlineQuery([game]);
 });
 bot.on('callback_query', async (ctx) => {
-  const gameUrl = `https://yohanz.com.et/index.php?userId=${userId}&chatId=${chatId}`;
+  
   const callbackQueryId = ctx.callbackQuery.id;
   const userId = ctx.callbackQuery.from.id;
   const chatId = ctx.callbackQuery.chat_instance;
+  const gameUrl = `https://yohanz.com.et/index.php?userId=${userId}&chatId=${chatId}`;
 
   console.log(`User ID: ${userId}`);
   console.log(`Chat ID: ${chatId}`);
